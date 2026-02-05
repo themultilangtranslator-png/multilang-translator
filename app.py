5073c87f8598284327bf6dee027493e16cf36832
themultilangtranslator-png <themultilangtranslator@gmail.com>
2026-02-04 22:19:53 -0500

    Update app.py
    Add root health endpoint

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def health():
+@app.route("/", methods=["GET"])
+def health_check():
     return "Translator service running", 200
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
