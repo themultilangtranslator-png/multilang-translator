c14e27ce6cbd32a9fc6d7090aae85677c35b0869
themultilangtranslator-png <themultilangtranslator@gmail.com>
2026-02-04 22:28:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def health_check():
     return "Translator service running", 200
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
